18547b2e6f155b748086f696b52289bc48558690
Andres Gomez <andres.gomez@commure.com>
2022-05-09 18:46:25 -0500

    feat: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import Card from '@mui/material/Card';
 import './App.css';
+import { Button, CardActions, CardContent, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography variant="body2">
+            Healthcare, meet the operating system designed just for you.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" variant='outlined'>Learn More</Button>
+          </CardActions>
+        </Card>
       </header>
     </div>
   );
